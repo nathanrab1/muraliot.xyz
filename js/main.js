ba63a639724b9ab2151b2c6ddcb6ffc0452497fe
nathanrab1 <85266894+nathanrab1@users.noreply.github.com>
2021-10-23 14:21:05 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function clicou1(){
   console.log(this.responseText);//should be return value of 1
   var oReq = new XMLHttpRequest();
  oReq.open("get", "https://api.thingspeak.com/update?api_key=LZO5NSZVQGPBNP32&field1=1", true);
-  oReq.onload = redirecionar();
+ // oReq.onload = redirecionar();
  // oReq.open("get", "179.113.61.116:1987/ledon", true);
  // oReq.send();
 }
@@ -11,7 +11,7 @@ function clicou2(){
   console.log(this.responseText);//should be return value of 1
   var oReq = new XMLHttpRequest();
  oReq.open("get", "https://api.thingspeak.com/update?api_key=LZO5NSZVQGPBNP32&field1=0", true);
-  oReq.onload = redirecionar();
+ // oReq.onload = redirecionar();
 //oReq.open("get", "179.113.61.116:1987/ledoff", true);
 //  oReq.send();
 }
